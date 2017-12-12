f0ac61bcad126d05965911e34d44e1a0e8c2e5ab
Michael Chlepakov <michael.chlepakov@gmail.com>
2017-12-12 19:25:49 +0100

    Fixed deleteAll and commented tests out for now since im not done yet.

--- source/core-logic/src/test/java/de/iosl/blockchain/identity/core/shared/claims/ClaimTest.java
+++ source/core-logic/src/test/java/de/iosl/blockchain/identity/core/shared/claims/ClaimTest.java
@@ -6,6 +6,7 @@ import de.iosl.blockchain.identity.core.shared.claims.provider.Provider;
 import de.iosl.blockchain.identity.core.shared.claims.repository.ClaimDB;
 import org.apache.tomcat.jni.Local;
 import org.assertj.core.util.Maps;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,15 +31,20 @@ public class ClaimTest {
     @Autowired
     public ClaimDB claimDB;
 
-    @Before
+    //@Before
     public void init(){
         payload = new Payload(content);
         provider = new Provider(ethID, providerName, providerPublicKey);
         claim = new Claim(id,createdDate,lastModifiedDate,payload,provider);
     }
 
-    @Test
+    //@Test
     public void saveClaimTest(){
         claimDB.saveClaim(claim);
+    }
+
+    //@After
+    public void clearDB(){
+        claimDB.deleteAll(Claim.class);
     }
 }
