3fd94dc39a55ca52443000659681188cb4fb2e0c
Timo Runck <timo-christoph.runck@mailbox.tu-berlin.de>
2017-12-17 16:00:05 +0100

    deployed registrar contract

---
+++ source/core-logic/src/main/resources/Registrar.sol
@@ -0,0 +1,42 @@
+pragma solidity ^0.4.11;
+
+contract FirstContract{
+
+    address owner;
+    address gov;
+    bool approval;
+
+    function FirstContract(){
+        owner=msg.sender;
+        gov=0x9A06cfA07F6D65d113ac9fcd1326355ae6DB1083;
+        approval=false;
+
+    }
+
+    modifier ifOwner(){
+        if(owner != msg.sender){
+            throw;
+        }
+        else{
+            _;
+        }
+    }
+
+    modifier ifGov(){
+        if(gov != msg.sender){
+            throw;
+        }
+        else{
+            _;
+        }
+    }
+
+    function getVariable() constant returns(bool){
+        return approval;
+    }
+
+    function setVariable(bool _approval) ifGov returns(bool){
+        approval = _approval;
+        return approval;
+    }
+}
