75d3486729e474cf20eccf108627c30f11e29ad0
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2017-12-25 15:39:46 +0100

    adapted seucirty config

--- source/provider/src/main/java/de/iosl/blockchain/identity/core/provider/config/SecurityConfig.java
+++ source/provider/src/main/java/de/iosl/blockchain/identity/core/provider/config/SecurityConfig.java
@@ -29,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                    .antMatchers("/user/**/register").permitAll()
+                    .antMatchers("/user/*/register").permitAll()
                     .antMatchers(ProviderAPIConstances.API_PATH + "/**").permitAll()
                     .anyRequest().fullyAuthenticated()
                 .and()
