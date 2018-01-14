822215d4d3af64478b2a0c8a4c42069e876f21dd
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2018-01-14 19:57:26 +0100

    made it into a profile

--- source/core-logic/src/main/java/de/iosl/blockchain/identity/core/shared/api/permission/data/dto/QueryRequestDTO.java
+++ source/core-logic/src/main/java/de/iosl/blockchain/identity/core/shared/api/permission/data/dto/QueryRequestDTO.java
@@ -2,11 +2,13 @@ package de.iosl.blockchain.identity.core.shared.api.permission.data.dto;
 
 import de.iosl.blockchain.identity.core.shared.api.data.dto.BasicEthereumDTO;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.NotBlank;
 
 @Data
 @NoArgsConstructor
+@EqualsAndHashCode(callSuper = true)
 public class QueryRequestDTO extends BasicEthereumDTO {
 
     @NotBlank
