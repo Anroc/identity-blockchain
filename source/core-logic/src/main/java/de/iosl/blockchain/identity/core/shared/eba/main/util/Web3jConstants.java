c0d0f557859b8aa40a06592777af83b5fff560ae
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2018-01-21 14:31:36 +0100

    updated contants2

--- source/core-logic/src/main/java/de/iosl/blockchain/identity/core/shared/eba/main/util/Web3jConstants.java
+++ source/core-logic/src/main/java/de/iosl/blockchain/identity/core/shared/eba/main/util/Web3jConstants.java
@@ -16,13 +16,13 @@ public class Web3jConstants {
 	@NotBlank
 	private String port;
 
-	public static final BigInteger GAS_PRICE = BigInteger.valueOf(20_000_000_000L);
+	public static final BigInteger GAS_PRICE = BigInteger.ZERO;
 	
 	public static final BigInteger GAS_LIMIT_ETHER_TX = BigInteger.valueOf(21_000);
 	public static final BigInteger GAS_LIMIT_GREETER_TX = BigInteger.valueOf(500_000L);
 	public static final BigInteger GAS_LIMIT_REGISTRAR_TX = BigInteger.valueOf(500_000L);
 
-	public static final int CONFIRMATION_ATTEMPTS = 40;
+	public static final int CONFIRMATION_ATTEMPTS = 5;
 	public static final int SLEEP_DURATION = 1000;
 
 	// file name extensions for smart contracts
