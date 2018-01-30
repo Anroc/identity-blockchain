6fdd7e32212bd01318e86f0746822a182d6e0073
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2018-01-30 23:14:25 +0100

    couchbase improvments

--- source/lib/src/main/java/de/iosl/blockchain/identity/lib/wrapper/CouchbaseWrapper.java
+++ source/lib/src/main/java/de/iosl/blockchain/identity/lib/wrapper/CouchbaseWrapper.java
@@ -1,16 +1,14 @@
 package de.iosl.blockchain.identity.lib.wrapper;
 
 import com.couchbase.client.java.Bucket;
-import com.couchbase.client.java.query.Delete;
-import com.couchbase.client.java.query.N1qlQueryResult;
-import com.couchbase.client.java.query.Statement;
+import com.couchbase.client.java.query.*;
+import com.couchbase.client.java.query.consistency.ScanConsistency;
 import de.iosl.blockchain.identity.lib.exception.ServiceException;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.couchbase.core.CouchbaseTemplate;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 
@@ -30,8 +28,6 @@ public class CouchbaseWrapper<T, ID extends Serializable> {
 
     @Autowired
     private Bucket bucket;
-    @Autowired
-    private CouchbaseTemplate couchbaseTemplate;
 
     public Optional<T> findEntity(@NonNull ID id) {
         return Optional.ofNullable(repository.findOne(id));
@@ -42,25 +38,27 @@ public class CouchbaseWrapper<T, ID extends Serializable> {
     }
 
     public T insert(T entity) {
-        couchbaseTemplate.insert(entity);
+        repository.save(entity);
         return entity;
     }
 
     public T update(T entity) {
-        couchbaseTemplate.update(entity);
-        return entity;
+        return insert(entity);
     }
 
     public void deleteAll(Class<T> clazz) {
         Statement statement =
                 Delete.deleteFrom(i(bucket.name()))
                         .where(i("_class").eq(s(clazz.getCanonicalName())));
-
         query(statement);
     }
 
     private N1qlQueryResult query(Statement statement) {
-        N1qlQueryResult result = bucket.query(statement);
+        N1qlQueryResult result = bucket.query(
+                N1qlQuery.simple(
+                        statement,
+                        N1qlParams.build().consistency(ScanConsistency.STATEMENT_PLUS))
+        );
         if (!result.finalSuccess()) {
             throw new ServiceException("Could not delete bucket. Reasons: %s",
                     HttpStatus.INTERNAL_SERVER_ERROR,
