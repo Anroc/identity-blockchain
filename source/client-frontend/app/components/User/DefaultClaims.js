4f1ea97bbd781a8fa11af24d733d24bd4ac67979
Oskar Schlösinger <o.schloesinger@gmx.de>
2018-02-05 16:38:40 +0100

    added default claims for more exhaustive testing

---
+++ source/client-frontend/app/components/User/DefaultClaims.js
@@ -0,0 +1,76 @@
+const DefaultClaims = [
+  {
+    modificationDate: 1515684492235,
+    claimValue: {
+      payloadType: 'STRING',
+      payload: 'Schloessinger',
+    },
+    id: 'FAMILY_NAME',
+    provider: {
+      name: 'Bürgeramt Reinickendorf',
+      ethID: '0xf1239acb6bb5e3a4a45de8cd14090c1944dce581',
+    },
+  },
+  {
+    modificationDate: 1515684492235,
+    claimValue: {
+      payloadType: 'DATE',
+      payload: '29.2.1994',
+    },
+    id: 'BIRTHDATE',
+    provider: {
+      name: 'Bürgeramt Reinickendorf',
+      ethID: '0xf1239acb6bb5e3a4a45de8cd14090c1944dce581',
+    },
+  },
+  {
+    modificationDate: 1515684492235,
+    claimValue: {
+      payloadType: 'STRING',
+      payload: 'Oskar',
+    },
+    id: 'GIVEN_NAME',
+    provider: {
+      name: 'Bürgeramt Reinickendorf',
+      ethID: '0xf1239acb6bb5e3a4a45de8cd14090c1944dce581',
+    },
+  },
+  {
+    modificationDate: 1515684492235,
+    claimValue: {
+      payloadType: 'STRING',
+      payload: 'Berlin',
+    },
+    id: 'BIRTHPLACE',
+    provider: {
+      name: 'Bürgeramt Reinickendorf',
+      ethID: '0xf1239acb6bb5e3a4a45de8cd14090c1944dce581',
+    },
+  },
+  {
+    modificationDate: 1515684492235,
+    claimValue: {
+      payloadType: 'STRING',
+      payload: 'B1GN0MB3R',
+    },
+    id: 'TAXID',
+    provider: {
+      name: 'Finanzamt Karottenburg',
+      ethID: '0xf1239acb6bb5e3a4a45de8cd14090c1944dce582',
+    },
+  },
+  {
+    modificationDate: 1515684492235,
+    claimValue: {
+      payloadType: 'STRING',
+      payload: 'Red Bull',
+    },
+    id: 'SPONSOR',
+    provider: {
+      name: 'RedBull Company',
+      ethID: '0xf1239acb6bb5e3a4a45de8cd14090c1944dce583',
+    },
+  },
+];
+
+export default DefaultClaims;
