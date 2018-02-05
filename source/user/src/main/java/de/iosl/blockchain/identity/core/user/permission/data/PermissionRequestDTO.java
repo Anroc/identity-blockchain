cc2b66b5628639e230962f29e62a7c4a67a87fa8
Marvin Petzolt <m.petzolt@campus.tu-berlin.de>
2018-02-05 20:47:32 +0100

    forgot DTO

--- source/user/src/main/java/de/iosl/blockchain/identity/core/user/permission/data/PermissionRequestDTO.java
+++ source/user/src/main/java/de/iosl/blockchain/identity/core/user/permission/data/PermissionRequestDTO.java
@@ -6,7 +6,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
 
-import javax.validation.constraints.NotNull;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
